0539d10f04759d6cca55a4bbd6e08e83384df46c
wesley-zorzolli <wesleysantoszeus483@gmail.com>
2025-10-16 10:39:28 -0300

    style: alterar fundo do login e cadastro para cinza claro (bg-gray-100)

--- src/CadCliente.tsx
+++ src/CadCliente.tsx
@@ -45,7 +45,7 @@ export default function CadCliente() {
     }
 
     return (
-        <section className="bg-gray-900 min-h-screen">
+        <section className="bg-gray-100 min-h-screen">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
--- src/Login.tsx
+++ src/Login.tsx
@@ -55,7 +55,7 @@ export default function Login() {
     }
 
     return (
-        <section className="bg-gray-50 dark:bg-white">
+        <section className="bg-gray-100 min-h-screen">
             <p style={{ height: 48 }}></p>
             <div className="flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
